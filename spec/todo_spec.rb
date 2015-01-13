804a7540e1deac5b49a0a513bea85cc77eb8776b
mgoren & dmmoody <student@epicodus.com>
2015-01-13 09:43:04 -0800

    add retrieve all descriptions spec

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -4,12 +4,23 @@ require('todo')
 describe(Task) do
 
   describe('#description') do
-
     it("lets you provide and returns a description") do
       test_task = Task.new("write a program")
       expect(test_task.description()).to(eq("write a program"))
     end
-
   end
+
+  describe('.all') do
+    it("gives an empty list of descriptions") do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+
+
+
+
+
+
+
 
 end
