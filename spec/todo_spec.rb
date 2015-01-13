63c20bf537849c9ec1dfe52e4876009db2b435d9
mgoren & dmmoody <student@epicodus.com>
2015-01-13 09:52:11 -0800

    add array of descriptions to test description method

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -14,16 +14,25 @@ describe(Task) do
     end
   end
 
+describe('.clear') do
+  it("clears list of descriptions") do
+    expect(Task.clear()).to(eq([]))
+  end
+end
+
   describe('.all') do
+
     it("gives an empty list of descriptions") do
       expect(Task.all()).to(eq([]))
     end
-  end
 
-  describe('.clear') do
-    it("clears list of descriptions") do
-      expect(Task.clear()).to(eq([]))
+    it("lists descriptions for all tasks") do
+      task1 = Task.new("write a program")
+      task2 = Task.new("drink coffee")
+      task3 = Task.new("eat food")
+      expect(Task.all()).to(eq(["write a program","drink coffee","eat food"]))
     end
+
   end
 
 
