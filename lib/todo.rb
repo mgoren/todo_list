b20b53a818eb1c73a9ee8eabdc334f2a9224076a
mgoren & dmmoody <student@epicodus.com>
2015-01-13 10:18:57 -0800

    add save method

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,20 +1,21 @@
 class Task
-  @@descriptions = []
+  @@tasks = []
 
   define_method(:initialize) do |description|
     @description = description
-    @@descriptions.push(description)
   end
 
   define_singleton_method(:clear) do
-    @@descriptions = []
+    @@tasks = []
   end
 
   define_singleton_method(:all) do
-    @@descriptions
+    @@tasks
   end
 
-
+  define_method(:save) do
+    @@tasks.push(self)
+  end
 
   define_method(:description) do
     @description
